e9a28fe56f3ebdcba76041346e571ab25c554f72
Kunal <kunalsonawane730@gmail.com>
2022-11-17 19:55:47 +0530

    adding starting position zero for snake and ladder game

--- src/main/java/com/bridgelabz/snakeandladder/SnakeAndLadder.java
+++ src/main/java/com/bridgelabz/snakeandladder/SnakeAndLadder.java
@@ -3,9 +3,9 @@ package com.bridgelabz.snakeandladder;
 public class SnakeAndLadder {
 
 	public static void main(String[] args) {
-
+//UC1 starting position is 0
 		System.out.println("Welcome to Snake and Ladder Game");
-		
+		int position =0;
 	}
 
 }
