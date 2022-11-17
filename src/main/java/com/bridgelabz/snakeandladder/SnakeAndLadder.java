77a10c9a9c68b2ec2d963608988c6b75dfc7b87f
Kunal <kunalsonawane730@gmail.com>
2022-11-17 20:09:24 +0530

    adding dice rolling randomly program

--- src/main/java/com/bridgelabz/snakeandladder/SnakeAndLadder.java
+++ src/main/java/com/bridgelabz/snakeandladder/SnakeAndLadder.java
@@ -3,9 +3,11 @@ package com.bridgelabz.snakeandladder;
 public class SnakeAndLadder {
 
 	public static void main(String[] args) {
-//UC1 starting position is 0
+		
 		System.out.println("Welcome to Snake and Ladder Game");
 		int position =0;
+		int randomCheck = (int) Math.floor(Math.random ()* 10) % 6+1;
+		System.out.println(randomCheck);
 	}
 
 }
