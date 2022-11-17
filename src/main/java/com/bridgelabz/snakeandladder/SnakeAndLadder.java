29a0c1e05a2e00922c915dd84f3a76de5c9f7b1f
Kunal <kunalsonawane730@gmail.com>
2022-11-17 20:25:31 +0530

    checking for option for no play ladder or snake

--- src/main/java/com/bridgelabz/snakeandladder/SnakeAndLadder.java
+++ src/main/java/com/bridgelabz/snakeandladder/SnakeAndLadder.java
@@ -3,11 +3,26 @@ package com.bridgelabz.snakeandladder;
 public class SnakeAndLadder {
 
 	public static void main(String[] args) {
+//UC3-randomly checking for option no play, ladder or snake
 		
 		System.out.println("Welcome to Snake and Ladder Game");
 		int position =0;
 		int randomCheck = (int) Math.floor(Math.random ()* 10) % 6+1;
-		System.out.println(randomCheck);
+		int randomCheck2 = (int) (Math.random()* 10 )% 3;
+		switch (randomCheck2) {
+			case 0:
+				System.out.println("No Play");
+				position +=0;
+				break;
+			case 1:
+				System.out.println("Ladder");
+				position +=randomCheck;
+				break;
+			case 2:
+				System.out.println("Snake");
+				position -=randomCheck;
+				break;
+		}
 	}
 
 }
